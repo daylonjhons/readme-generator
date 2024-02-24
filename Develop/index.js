28f40436a01bebbcdedd4389f90638dcdb4fd77c
daylonjhons <daylonjhons@gmail.com>
2024-02-23 21:10:20 -0600

    added markdown gen and added functions to index

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,81 @@
-// TODO: Include packages needed for this application
+// Includes packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username ?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address ?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project\'s name ?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have ?',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies ?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests ?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo ?'
+    },
+    {
+        type: 'confirm',
+        name: 'contributing',
+        message: 'Would you like to allow people to contribute to this project ?'
+    },
+    {
+        // Additional question about how to contribute, only asked if the user chooses to allow contributions
+        when: function(answers) {
+            return answers.contributing;
+        },
+        name: "howToContribute",
+        type: "input",
+        message: "Please enter a description of how to contribute to this project:"
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function writes README file
+function writeToFile(fileName, data) {
+    writeFile(fileName, data, (err) => { 
+        console.log('README file has been created.');
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function initializes app .... using async/await rather than .then/catch
+async function init() {
+    const answers = await inquirer.prompt(questions);
 
-// Function call to initialize app
+    writeToFile('README.md', generateMarkdown(answers));
+}
+
+// Function calls to initialize app
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,103 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to generate markdown for README
+const generateMarkdown = data => {
+  // License section
+  const licenseSection = renderLicenseSection(data.license);
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  // Contribution section
+  let contributionSection = '';
+  if (data.contributing === true && data.howToContribute) {
+      contributionSection = `## Contributing
+${data.howToContribute}
+`;
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+  // Generating the markdown
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](##Installation)
+- [Usage](##Usage)
+- [Test](#Test)
+- [License](##License)
+- [Questions](##Questions)
+${contributionSection ? '- [Contributing](##Contributing)\n' : ''}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Test
+${data.test}
+
+${licenseSection}
+
+${contributionSection ? contributionSection : ''}
+
+## Questions:
+If you have any questions about the repo, you can message me @ [email]${data.email}. 
+You can also find me on Github @ [${data.github}](https://github.com/${data.github}).
 `;
 }
 
+// Function to render the license section
+const renderLicenseSection = license => {
+  if (!license) return '';
+
+  return `## License
+
+Licensed under the ${license} license.
+
+${renderLicenseBadge(license)}\
+${renderLicenseLink(license)}`
+}
+
+// Function to render the license badge
+const renderLicenseBadge = license => {
+  if (!license) return '';
+
+  switch (license) {
+      case 'The MIT License':
+          return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      case 'Apache 2.0 License':
+          return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      case 'GNU GPL v3':
+          return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      case 'Mozilla Public License 2.0':
+          return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      case 'BSD 3-Clause License':
+          return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+      default:
+          return '';
+  }
+}
+
+// Function to render the license link
+const renderLicenseLink = license => {
+  if (!license) return '';
+
+  switch (license) {
+      case 'The MIT License':
+          return '(https://opensource.org/licenses/MIT)';
+      case 'Apache 2.0 License':
+          return '(https://opensource.org/licenses/Apache-2.0)';
+      case 'GNU GPL v3':
+          return '(https://www.gnu.org/licenses/gpl-3.0)';
+      case 'Mozilla Public License 2.0':
+          return '(https://opensource.org/licenses/MPL-2.0)';
+      case 'BSD 3-Clause License':
+          return '(https://opensource.org/licenses/BSD-3-Clause)';
+      default:
+          return '';
+  }
+}
+
+// Exporting the generateMarkdown function
 module.exports = generateMarkdown;
+
